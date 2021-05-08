3192b4f104d08be887b74c8558a10d858fc1e9db
Albert Canales Ros <albertcanalesros@gmail.com>
2021-05-08 19:01:11 +0200

    Fixed plot and gitignore added

--- igo.py
+++ igo.py
@@ -3,22 +3,24 @@ import networkx as nx
 import osmnx
 import matplotlib.pyplot as plt
 import os.path
+import pickle
 
 PLACE = 'Barcelona, Catalonia'
+IMAGE_FILENAME = 'barcelona.png'
 GRAPH_FILENAME = 'barcelona.graph'
 SIZE = 800
 HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-1c40-4609-8620-14ad49aae3ab/resource/1d6c814c-70ef-4147-aa16-a49ddb952f72/download/transit_relacio_trams.csv'
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
-Highway = collections.namedtuple('Highway', '...') # Tram
-Congestion = collections.namedtuple('Congestion', '...')
+Highway = collections.namedtuple('Highway', 'a') # Tram
+Congestion = collections.namedtuple('Congestion', 'a')
 
 def exists_graph(filename):
     return os.path.isfile(filename)
 
 def download_graph(place):
     graph = osmnx.graph_from_place(place, network_type='drive', simplify=True)
-    graph = osmnx.utils_graph.get_digraph(graph, weight='length')
+    #graph = osmnx.utils_graph.get_digraph(graph, weight='length')
     return graph
 
 def save_graph(graph, filename):
@@ -30,8 +32,11 @@ def load_graph(filename):
         graph = pickle.load(file)
     return graph
 
-def plot_graph():
-    nx.draw()
+def plot_graph(graph, save=True):
+    if save:
+        osmnx.plot_graph(graph, save=True, filepath=IMAGE_FILENAME)
+    else:
+        osmnx.plot_graph(graph)
 
 def download_csv(url):
     with urllib.request.urlopen(url) as response:
@@ -56,8 +61,10 @@ def test():
     if not exists_graph(GRAPH_FILENAME):
         graph = download_graph(PLACE)
         save_graph(graph, GRAPH_FILENAME)
+        print("Graph generated")
     else:
         graph = load_graph(GRAPH_FILENAME)
+        print("Graph loaded")
     plot_graph(graph)
 
     # download highways and plot them into a PNG image
@@ -74,3 +81,5 @@ def test():
     # get 'intelligent path' between two addresses and plot it into a PNG image
     ipath = get_shortest_path_with_ispeeds(igraph, "Campus Nord", "Sagrada Família")
     plot_path(igraph, ipath, SIZE)
+
+test()
